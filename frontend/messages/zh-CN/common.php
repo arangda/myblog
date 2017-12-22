c9352d2e31b509c2a925c38b760a05fad8f56b1a
arangda <273890638@qq.com>
2017-12-22 14:28:25 +0800

    first

---
+++ frontend/messages/zh-CN/common.php
@@ -0,0 +1,15 @@
+<?php
+
+	return array(
+		'Blog' => '阿让达',
+		'Home' => '首页',
+		'About' => '关于',
+		'Contact' => '联系',
+		'Logout' => '退出',
+		'Login' => '登录',
+		'Signup' => '注册',
+		'Email' =>'电子邮箱',
+		'Password' =>'密码',
+	);
+
+?>
